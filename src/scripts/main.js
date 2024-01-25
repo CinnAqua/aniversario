32040756030da252c2484d1d8b4fa7e58a87461b
CinnAqua <miguelcp1008@gmail.com>
2024-01-25 16:11:20 -0300

    mudei o tema do site

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,6 +1,6 @@
 AOS.init();
 
-const dataDoEvento = new Date("Feb 12, 2024 19:00:00");
+const dataDoEvento = new Date("Oct 10, 2024 20:00:00");
 const timeStampDoEvento = dataDoEvento.getTime();
 
 const contaAsHoras = setInterval(function() {
